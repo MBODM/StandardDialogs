7fadb90730b943c0de947835aedcea8e9a5e5e34
MBODM <webmaster@mbodm.com>
2016-07-16 07:57:21 +0200

    Initial commit

---
+++ MBODM.WPF.StandardDialogs/MBODM.WPF.StandardDialogs/Sourcecode/Public/Interfaces/IStandardDialogs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBODM.WPF
+{
+    public interface IStandardDialogs
+    {
+        void ShowInfoMessage(string message);
+        void ShowWarningMessage(string message);
+        void ShowErrorMessage(string message);
+        IEnumerable<string> ShowOpenFileDialog(string folder, string filter, bool multiselect);
+        string ShowSaveFileDialog(string folder, string filter);
+        string ShowFolderBrowserDialog(string folder);
+    }
+}
